5b20c8bb9fd3aeb2bafe3fc11505a261836acc08
wulishuai <wulitu1984@163.com>
2024-05-14 16:01:23 +0800

    resnet acc top1 done

--- resnet18/src/main.cpp
+++ resnet18/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector>
-#include <thread>
 #include <string>
 #include <unistd.h>
 
@@ -15,57 +16,85 @@
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb/stb_image_resize.h"
 
+static int RGB2BGR(unsigned char* buf, int width, int height) {
+	unsigned char tmp;
+	for (int i = 0; i < width * height * 3; i += 3)
+	{
+		tmp = buf[i];
+		buf[i] = buf[i + 2];
+		buf[i + 2] = tmp;
+	}
+	return 0;
+}
 
-int resnet_demo(std::string path)
+int resnet_demo(hcnn2::NetEZ82x &net, std::string path)
 {
 	int w, h, n;
 	unsigned char* imgbuf = stbi_load(path.c_str(), &w, &h, &n, 0);
-	spdlog::info("readpic w {}, h {}", w, h);
+	// spdlog::info("image {}, w {}, h {}, n {}", path, w, h, n);	
+	if (n == 3)
+		RGB2BGR((unsigned char*)imgbuf, w, h);
 
 	std::vector<int> shape = {1, h, w, 3};
 	hcnn2::Blob blob_in("data", hcnn2::BlobType::RGB24, shape, 
 					{(void*)imgbuf, (void*)((unsigned char*)imgbuf+w*h)});
 
-	hcnn2::NetEZ82x net;
-	net.load("./models/resnet18.ezb", "./models/resnet18.bin");
-
-	hcnn2::NetConfig config = {true, hcnn2::NetDataLayout::NWHC};
-	net.setConfig(config);
-
 	if (net.input(blob_in) != 0) {
 		spdlog::error("set input error!");
-		net.unload();
 		free(imgbuf);
 		return -1;
 	}
 
 	std::vector<hcnn2::Blob> blobs_out = net.forward();
 
-	for (auto blob_out : blobs_out) {
-		spdlog::info("net output:  {}-{}-{}-{}", 
-					blob_out.shape[0], blob_out.shape[1], blob_out.shape[2], blob_out.shape[3]);
-		hcnn2::Mat<float> output((float*)blob_out.data[0], {1,1,1,1000});
-		output.tofile("output.bin");
-		int max_id = -1;
-		float max = 0;
-		for(int i=0;i<1000;++i) {
-			if (output(0,0,0,i) > max) {
-				max_id = i;
-				max = output(0,0,0,i);
-			}
+	auto blob_out = blobs_out[0];
+	hcnn2::Mat<float> output((float*)blob_out.data[0], {1,1,1,1000});
+	// output.tofile("output.bin");
+	int max_id = -1;
+	float max = 0;
+	for(int i=0;i<1000;++i) {
+		if (output(0,0,0,i) > max) {
+			max_id = i;
+			max = output(0,0,0,i);
 		}
-		spdlog::info("max:  {}-{}", max_id, max);
 	}
 
-
-	net.unload();
 	free(imgbuf);
-	return 0;
+	return max_id;
 }
 
 int main(int argc, char** argv)
 {
-	resnet_demo(std::string(argv[1]));
+	hcnn2::NetEZ82x net;
+	net.load("./models/resnet18.ezb", "./models/resnet18.bin");
+	hcnn2::NetConfig config = {true, hcnn2::NetDataLayout::NWHC};
+	net.setConfig(config);
+
+	std::ifstream ifs("./labels/val.txt");
+	std::string line;
+	int total = 0;
+	int correct = 0;
+	float acc_top1 = 0;
+
+	while (std::getline(ifs, line))
+    {
+    	std::istringstream iss(line);
+		std::string fname;
+		iss >> fname;
+		int cls;
+		iss >> cls;
+		int pred = resnet_demo(net, "./images/"+fname);
+		spdlog::info("image {}, label {}, pred {}", fname, cls, pred);	
+		if (pred == cls) {
+			correct++;
+		}
+		total++;
+	}
+
+	acc_top1 = (float)correct / total;
+	spdlog::info("total {}, correct {}, acc_top1 {}", total, correct, acc_top1);
+
+	net.unload();
 	return 0;
 }
 
